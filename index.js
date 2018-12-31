7b6f0c708c1dc2744f5d8894c29823d31307c341
Al3nMicL <https://twitter.com/Al3nMicL>
2018-12-31 16:07:12 -0500

    first half of tests pass

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+// appends a kitten to the end of the kittens array
+var destructivelyAppendKitten = (name) => {
+	return kittens.push(name);
+};
+
+// prepends a kitten to the beginning of the kittens array
+var destructivelyPrependKitten = (name) => {
+	return kittens.unshift(name);
+};
+
+// removes the last kitten from the kittens array
+var destructivelyRemoveLastKitten = (name) => {
+	return kittens.pop(name);
+};
+
+// removes the First kitten from the kittens array
+var destructivelyRemoveFirstKitten = (name) => {
+	return kittens.shift(name);
+};
